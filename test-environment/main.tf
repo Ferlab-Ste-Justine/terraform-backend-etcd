0e2176a6060b8cbbe77acca078180c0b585f814f
Eric Vallee <eric_vallee@webificservices.com>
2022-10-05 14:40:13 -0400

    INFRA-129 Added a few more resources as extra validation

--- test-environment/main.tf
+++ test-environment/main.tf
@@ -1,5 +1,13 @@
 resource "local_file" "hello" {
-  content         = "hello8"
+  count = 10
+  content         = "hello${count.index}"
   file_permission = "0660"
-  filename        = pathexpand("${path.module}/output/hello")
+  filename        = pathexpand("${path.module}/output/hello${count.index}")
+}
+
+resource "local_file" "goodbye" {
+  count = 10
+  content         = "goodbye${count.index}"
+  file_permission = "0660"
+  filename        = pathexpand("${path.module}/output/goodbye${count.index}")
 }
